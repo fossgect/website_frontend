bcd4c2b50f30e079598f4dd671694803411ca59b
JithinKS97 <jithunni.ks@gmail.com>
2018-12-03 01:43:45 +0530

    jquery scroll added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import $ from "jquery";
 
 class App extends Component {
+
+
+
+  componentDidMount() {
+
+    $('div.top').click(function() {
+
+      $('html, body').animate({
+        scrollTop: $("div.middle").offset().top
+      }, 1000)
+    })
+
+    $('div.middle').click(function (){
+      $('html, body').animate({
+        scrollTop: $("div.bottom").offset().top
+      }, 1000)
+    })
+
+    $('div.bottom').click(function (){
+      $('html, body').animate({
+        scrollTop: $("div.top").offset().top
+      }, 1000)
+    })
+
+    $('div.top').on('scroll', function() {
+
+      console.log('hee')
+    })
+  }
+
   render() {
+    console.log($);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div style = {{height:'100vh'}} className="top">
+          <h1>Top</h1>
+        </div>
+        <div style = {{height:'100vh'}} className="middle">
+          <h1>Middle</h1>
+        </div>
+        <div style = {{height:'100vh'}} className="bottom">
+          <h1>Bottom</h1>
+        </div>
+      </Fragment>
     );
   }
 }
